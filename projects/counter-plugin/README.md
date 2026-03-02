4f14b4e8b6d03721ff302be05aa7e389b3c5a0a0
Nawaz <developerr2017@gmail.com>
2026-03-02 06:32:31 +0530

    -nawaz

--- projects/counter-plugin/README.md
+++ projects/counter-plugin/README.md
@@ -18,3 +18,72 @@ npm install counter-plugin
 ![GitHub stars](https://img.shields.io/github/stars/devloper2017/counter-plugin)
 ![GitHub issues](https://img.shields.io/github/issues/devloper2017/counter-plugin)
 ![GitHub license](https://img.shields.io/github/license/devloper2017/counter-plugin)
+
+
+for Angular
+
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from '@nawazaideveloperr/counter-plugin';
+
+@NgModule({
+  imports: [BrowserModule, CounterModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+
+<lib-counter [start]="0" [end]="1000" [timer]="2000" [step]="2"></lib-counter>
+
+
+for React
+
+import React from 'react';
+import { Counter } from '@nawazaideveloperr/counter-plugin';
+
+function App() {
+  return (
+    <div>
+      <h1>Visitors</h1>
+      <Counter start={0} end={500} timer={1500} step={5} />
+    </div>
+  );
+}
+
+export default App;
+
+
+for Vanilla JS
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Counter Demo</title>
+  <script src="node_modules/@nawazaideveloperr/counter-plugin/dist/counter-plugin.umd.js"></script>
+</head>
+<body>
+  <h1 id="counter"></h1>
+
+  <script>
+    const counter = new Counter({
+      element: document.getElementById('counter'),
+      start: 0,
+      end: 300,
+      timer: 2000,
+      step:10
+    });
+    counter.start();
+  </script>
+</body>
+</html>
+
+
+## ⚙️ Options
+
+| Prop       | Type    | Default | Description                          |
+|------------|---------|---------|--------------------------------------|
+| `start`    | number  | 0       | Starting number of the counter       |
+| `end`      | number  | 100     | Ending number of the counter         |
+| `timer` | number  | 2000    | Animation duration in milliseconds   |
+| `step`     | number  | auto    | Increment step size (calculated auto)|
--- projects/counter-plugin/src/lib/counter.component.ts
+++ projects/counter-plugin/src/lib/counter.component.ts
@@ -1,32 +1,74 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'lib-counter',
-  template: `<p>{{ current }}</p>`
+  template: `<p>{{ current }}</p>`,
 })
-export class CounterComponent implements OnInit, OnDestroy {
-  @Input() start = 0;   // starting value
-  @Input() end = 100;   // ending value
-  @Input() step = 1;    // increment step
-  @Input() timer = 1000; // interval in ms
+export class CounterComponent implements AfterViewInit {
+  // @Input() start = 0;   // starting value
+  // @Input() end = 100;   // ending value
+  // @Input() step = 1;    // increment step
+  // @Input() timer = 1000; // interval in ms
 
-  current!: number;
-  private intervalId: any;
+  // current!: number;
+  // private intervalId: any;
 
-  ngOnInit() {
-    this.current = this.start;
-    this.intervalId = setInterval(() => {
-      if (this.current + this.step <= this.end) {
-        this.current += this.step;
-      } else {
-        clearInterval(this.intervalId);
-      }
-    }, this.timer);
+  // ngOnInit() {
+  //   this.current = this.start;
+  //   this.intervalId = setInterval(() => {
+  //     if (this.current + this.step <= this.end) {
+  //       this.current += this.step;
+  //     } else {
+  //       clearInterval(this.intervalId);
+  //     }
+  //   }, this.timer);
+  // }
+
+  // ngOnDestroy() {
+  //   if (this.intervalId) {
+  //     clearInterval(this.intervalId);
+  //   }
+  // }
+
+  @Input() start = 0;
+  @Input() end = 1000;
+  @Input() timer = 1000; // total timer in ms
+  @Input() step = 5; // kitna jump kare (default 5)
+
+  current = 0;
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.animateCounter();
+            this.observer.disconnect(); // run only once
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    this.observer.observe(this.el.nativeElement);
   }
 
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+  animateCounter() {
+    const range = this.end - this.start;
+    const steps = Math.ceil(range / this.step);
+    const stepTime = Math.max(Math.floor(this.timer / steps), 10);
+
+    let value = this.start;
+    const timer = setInterval(() => {
+      value += this.step;
+      if (value >= this.end) {
+        value = this.end;
+        clearInterval(timer);
+      }
+      this.current = value;
+    }, stepTime);
   }
 }
